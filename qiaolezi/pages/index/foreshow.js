74b11f165284e65c942f37a74fafab29ce08a9f2
吴帅鹏 <shuaipeng@wushuang.me>
2019-02-25 17:57:24 +0800

    弹窗

--- qiaolezi/pages/index/foreshow.js
+++ qiaolezi/pages/index/foreshow.js
@@ -3,16 +3,41 @@
 const app = getApp()
 Page({
   data: {
-   
+    code:8888888888888,
+    isPopCdkey:false,
+    isPopHint:false
+  },
+  onClose(e){
+    this.setData({
+      isPopCdkey:false,
+      isPopHint:false
+    })
   },
   onLoad:function(query) {
     const scene = decodeURIComponent(query.q)
+    // const scene = "https://qiaolezi.act.qq.com/e/c/code/5";
+    // const scene = "https://qiaolezi.act.qq.com/e/c/code/XXXXXXXYYYYYY";
+    // const scene = "https://qiaolezi.act.qq.com/e/c/code/"
     if (!scene || scene == 'undefined') return false
+    this.onIf(scene)
+  },
+  onIf(scene){
+    var _this = this;
     var arr = scene.split("/");
-    // wx.showToast({
-    //   title: arr[arr.length - 1],
-    //   icon: 'none',
-    //   duration: 5000
-    // })
+    var code = arr[arr.length - 1]
+    console.log(code)
+    if(code.length == 13){
+      _this.setData({
+        code:code,
+        isPopCdkey:true
+      })
+    }else if(code<=5 && code >0){
+      //指定五个地址
+    }else{
+      //hint
+      _this.setData({
+        isPopHint:true
+      })
+    } 
   }
 })
